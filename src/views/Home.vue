1ad49a2984efe24ff0ddc53d5786a5a4d5481345
Bart Stéphane <stephane.bart@he-arc.ch>
2020-08-04 20:12:51 +0200

    update home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,14 @@
 <template>
   <div class="home">
     <h1>Test API Evento</h1>
-    <button @click="loadCats">Give me cats</button>
-    <div v-for="cat in cats" :key="cat.id" class="cat">
-      <img :src="cat.url"/>
-    </div>
   </div>
 </template>
 <script>
-  import axios from 'axios';
 export default {
   data() {
     return {
-      cats: []
+      test: ""
     };
-  },
-  methods: {
-    loadCats() {
-      axios
-        .get("https://api.thecatapi.com/v1/images/search?limit=6", {
-          headers: {
-            "x-api-key": "DEMO-API-KEY"
-          }
-        })
-        .then(response => {
-          console.log(response.headers);
-          console.log(response.data);
-          this.cats = response.data;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
   }
 };
 </script>
