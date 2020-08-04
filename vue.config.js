8b1d4e6880e98cc6a637b8c89406ed48fe0c6cf5
Bart Stéphane <stephane.bart@he-arc.ch>
2020-08-04 20:07:36 +0200

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/bartstephane-mycv/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "gymappapi/" : "/"
 };
